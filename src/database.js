a241024755a4673d0d76eb673a22a360ba7ffdb9
Alejandro Roa <alejodev04@gmail.com>
2020-05-18 08:44:14 -0400

    update item

--- src/database.js
+++ src/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://localhost/clients-db', {
+mongoose.connect(process.env.MONGO_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false
